d4ce6f45155c4ff346831a19adb0eb8db7055364
Claudinei Granemann Auersvald <298454658+Cauersvald-hash@users.noreply.github.com>
2026-07-16 00:07:33 +0000

    alteracoes de posicoes

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,31 +2,43 @@
 
 int main(){
 
-    // Declaracoes de Variaveis
+    // Declaracoes
 
-    int opcao;
-    int index;
     int tabuleiro[10][10] = {0};
 
     // Vetores
 
-    int navio_x[3] = {3, 3, 3};
-    int navio_y[3] = {3, 3, 3};
+    // int navio_x[3] = {3, 3, 3};
+    // int navio_y[3] = {3, 3, 3};
 
     // Coordenadas
 
-    int linha_x = 2;
-    int coluna_x = 4;
+    int navio_HL = 1;
+    int navio_HC  = 5;
 
-    int linha_y = 5;
-    int coluna_y = 7;
+    int navio_VL = 5;
+    int navio_VC = 7;
+
+    int navio_DL = 2;
+    int navio_DC = 2;
+
+    int navio_DL2 = 6;
+    int navio_DC2 = 4;
 
 // Loop contador de lacunas
 
-    for (index = 0; index < 3; index++)
+    for (int i = 0; i < 3; i++)
     {
-     tabuleiro[linha_x][coluna_x + index] = navio_x[index];
-     tabuleiro[linha_y + index][coluna_y] = navio_y[index];
+     tabuleiro[navio_HL][navio_HC + i] = 3; // exclui vetotes e simplifiquei a identificacao dos navios
+     tabuleiro[navio_VL + i][navio_VC] = 3;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[navio_DL + i][navio_DC + i] = 3;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[navio_DL2 + i][navio_DC2 - i] = 3;
     }
 
     // Contador de linhas (0 a 9(A a J))
@@ -35,7 +47,6 @@ int main(){
      {
 
     // Contador de colunas (0 a 9)
-    
      for (int col = 0; col < 10; col++)
      {
     // Escrevendo os resultados linhas e colunas
